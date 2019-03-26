aafa27b70c0b73b5b7d1c63bc6b4ffcf2f1f02aa
maxlamberti <maximilien.lamberti@gmail.com>
2019-03-26 00:09:11 -0700

    add tesla, walmart and apple urls from sec edgar database

---
+++ data/sec_edgar_urls.py
@@ -0,0 +1,7 @@
+
+
+URLS_10K = {
+	'APPLE_18': "https://www.sec.gov/Archives/edgar/data/320193/000032019318000145/a10-k20189292018.htm",
+	'TESLA_18': "https://www.sec.gov/Archives/edgar/data/1318605/000156459019003165/tsla-10k_20181231.htm",
+	'WALMART_18': "https://www.sec.gov/Archives/edgar/data/104169/000010416918000028/wmtform10-kx1312018.htm"
+}
