106f5a309ba15efe100586ba3ce820eb2bb0fa6a
maxlamberti <maximilien.lamberti@gmail.com>
2019-03-28 14:00:01 -0700

    add custom stopwords

---
+++ utils/stopwords.py
@@ -0,0 +1,10 @@
+
+
+stopwords = [
+    'chevron', 'apple', 'microsoft', 'amazon',
+    'facebook', 'alphabet', 'visa', 'united',
+    'chevron', 'pfizer', 'cisco', 'depot', 'merck',
+    'mastercard', 'disney', 'philip', 'company', 'business',
+	'unitedhealthcare', 'product', 'market', 'million', 'billion',
+    'percent', 'project', 'report', 'googl'
+]
